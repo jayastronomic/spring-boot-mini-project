a2b7ae0e6f65424bdc0fabcae1a093d87f673a10
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 14:54:40 -0500

    Create Book Service

---
+++ src/main/java/com/example/library/repositories/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.library.repositories;
+
+import com.example.library.models.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
