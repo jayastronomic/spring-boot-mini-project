7b5e1deb1aefc002a5faae9561b70c7decfd2c8d
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 19:29:54 -0500

    AuthUserDetails

--- src/main/java/com/example/library/security/AuthUserDetails.java
+++ src/main/java/com/example/library/security/AuthUserDetails.java
@@ -1,6 +1,44 @@
 package com.example.library.security;
 
+import com.example.library.models.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public record AuthUserDetails implements UserDetails(User user) {
+import java.util.Collection;
+
+public record AuthUserDetails(User user)  implements UserDetails  {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
