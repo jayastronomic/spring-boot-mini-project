bc3cd9f2999ba4607d5f7625be55357e3bb781b8
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:42:42 -0500

    Add updateAt field in ApplicationEntity class

--- src/main/java/com/example/library/models/ApplicationEntity.java
+++ src/main/java/com/example/library/models/ApplicationEntity.java
@@ -3,6 +3,7 @@ package com.example.library.models;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -22,4 +23,10 @@ public class ApplicationEntity<T> {
     @Temporal(TemporalType.TIMESTAMP)
     @CreatedDate
     private Date createdAt;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 }
