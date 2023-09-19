b91b60078cb6ad81cad744ab4a3ef997b4e26ddc
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:55:27 -0500

    Add validations to User class

--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -6,6 +6,9 @@ import lombok.EqualsAndHashCode;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -13,11 +16,17 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class User extends ApplicationEntity<User>{
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Username can not be blank")
+    @Size(min = 2, max = 300, message = "Username must be between 2 and 300 characters")
     private String username;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email can not be blank")
+    @Email(message = "Invalid email format")
+    @Size(max = 300, message = "Email must be less than 300 characters")
     private String email;
 
     @Column(nullable = false, unique = true)
+    @NotBlank(message = "Password can not be blank")
     private String password;
 }
