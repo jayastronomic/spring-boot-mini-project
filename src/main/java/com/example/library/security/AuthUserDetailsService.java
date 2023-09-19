ebe95027fe894f74ce26158d72752e6edbafee27
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 19:37:42 -0500

    Create AuthUserDetailsService

---
+++ src/main/java/com/example/library/security/AuthUserDetailsService.java
@@ -0,0 +1,10 @@
+package com.example.library.security;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthUserDetailsService implements UserDetailsService {
+
+}
