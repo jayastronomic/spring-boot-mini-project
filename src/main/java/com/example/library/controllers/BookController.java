9a2ca3527cef376b7e99b312ca7ce6589484cc51
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 15:14:51 -0500

    Autowire dependencies to  Book Controller

--- src/main/java/com/example/library/controllers/BookController.java
+++ src/main/java/com/example/library/controllers/BookController.java
@@ -1,4 +1,17 @@
 package com.example.library.controllers;
 
+import com.example.library.services.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
 public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
 }
