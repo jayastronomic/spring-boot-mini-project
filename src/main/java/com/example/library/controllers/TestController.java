8ba5dec4b5bd6ea794ea67462b3654261707d9d9
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 10:58:50 -0500

    Create Test controller

--- src/main/java/com/example/library/controllers/TestController.java
+++ src/main/java/com/example/library/controllers/TestController.java
@@ -1,4 +1,21 @@
 package com.example.library.controllers;
 
+import com.example.library.utils.APIResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api/v1/test")
 public class TestController {
+
+    @GetMapping
+    public ResponseEntity<APIResponse> test(){
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "active");
+        return ResponseEntity.ok(new APIResponse(response));
+    }
 }
