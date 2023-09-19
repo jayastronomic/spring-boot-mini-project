63169f380b97730287c7b7dea498cee7ba319bfa
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 00:04:37 -0500

    Extend UserRepository interface with JpaRepository

--- src/main/java/com/example/library/repositories/UserRepository.java
+++ src/main/java/com/example/library/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.library.repositories;
 
-public class UserRepository {
+import com.example.library.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
 }
