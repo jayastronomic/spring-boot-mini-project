1e5e14352f1165584a685edefc2897e375168c07
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:36:19 -0500

    Implement CorsPolicy::addCorsMappings

--- src/main/java/com/example/library/security/CorsPolicy.java
+++ src/main/java/com/example/library/security/CorsPolicy.java
@@ -1,4 +1,19 @@
 package com.example.library.security;
 
-public class CorsPolicy {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsPolicy implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
