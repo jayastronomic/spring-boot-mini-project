122948a0fb5a100fb171663546eb31d875de60a9
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 00:13:04 -0500

    Create Autowired AuthService constructor

--- src/main/java/com/example/library/services/AuthService.java
+++ src/main/java/com/example/library/services/AuthService.java
@@ -2,6 +2,11 @@ package com.example.library.services;
 
 import com.example.library.models.User;
 import com.example.library.repositories.UserRepository;
+import com.example.library.security.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -10,5 +15,16 @@ import java.util.logging.Logger;
 public class AuthService {
     Logger logger = Logger.getLogger(AuthService.class.getName());
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
+    @Autowired
+    public AuthService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder,
+                       JWTUtils jwtUtils, @Lazy AuthenticationManager authenticationManager){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+    }
 }
