69ad8723e52c8dfa11b68f45546d7252127bcab1
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:40:46 -0500

    Enable Auditing in ApplicationEntity class

--- src/main/java/com/example/library/models/ApplicationEntity.java
+++ src/main/java/com/example/library/models/ApplicationEntity.java
@@ -1,17 +1,18 @@
 package com.example.library.models;
 
 import lombok.Data;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class ApplicationEntity<T> {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     public UUID id;
+
+
 }
--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -1,14 +1,16 @@
 package com.example.library.models;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User extends ApplicationEntity<User>{
     private String username;
     private String email;
     private String password;
