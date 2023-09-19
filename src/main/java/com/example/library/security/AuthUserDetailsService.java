895bd8eb089958b2e098967a4e97fdefbef7e11e
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 19:38:32 -0500

    Create AuthUserDetailsService Autowired constructor

--- src/main/java/com/example/library/security/AuthUserDetailsService.java
+++ src/main/java/com/example/library/security/AuthUserDetailsService.java
@@ -1,10 +1,15 @@
 package com.example.library.security;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
+    private final UserService userService;
 
+    @Autowired
+    public AuthUserDetailsService(UserService userService){
+        this.userService = userService;
+    }
 }
