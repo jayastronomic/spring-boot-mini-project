5c68a2025940882bd27688c3107b130abfbb6d3a
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 14:32:37 -0500

    Add validations to Book model

--- src/main/java/com/example/library/models/Book.java
+++ src/main/java/com/example/library/models/Book.java
@@ -1,25 +1,32 @@
 package com.example.library.models;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "books")
 public class Book extends ApplicationEntity<Book> {
     @Column(nullable = false)
+    @NotBlank(message = "Title can not be blank")
     private String title;
 
     @Column(nullable = false)
+    @NotBlank(message = "Pages can not be blank")
     private Integer pages;
 
     @Column(nullable = false)
+    @NotBlank(message = "Author can not be blank")
     private String author;
 
     @Column(nullable = false)
+    @NotBlank(message = "Summary can not be blank")
     private String summary;
 
     @Column(nullable = false)
