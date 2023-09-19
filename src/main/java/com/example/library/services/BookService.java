5efdab14d65e8e942e0c7075298f3a323026107f
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 15:12:27 -0500

    Create BookService::createBook method

--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -1,7 +1,10 @@
 package com.example.library.services;
 
+import com.example.library.models.Book;
 import com.example.library.repositories.BookRepository;
+import com.example.library.utils.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.logging.Logger;
@@ -14,6 +17,12 @@ public class BookService extends ApplicationService {
     @Autowired
     public BookService(BookRepository bookRepository){
         this.bookRepository = bookRepository;
+    }
+
+    public ResponseEntity<APIResponse> createBook(Book book){
+        book.setUser(currentUser());
+        return ResponseEntity
+                .ok(new APIResponse(bookRepository.save(book)));
     }
 
 }
