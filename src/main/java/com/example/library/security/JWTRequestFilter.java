37e191a8480353f4a60e0ef27075fb5be6ed43d1
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:27:35 -0500

    Create JWTRequestFilter Autowired constructor

--- src/main/java/com/example/library/security/JWTRequestFilter.java
+++ src/main/java/com/example/library/security/JWTRequestFilter.java
@@ -1,9 +1,19 @@
 package com.example.library.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.logging.Logger;
 
+@Component
 public class JWTRequestFilter {
     Logger logger = Logger.getLogger(JWTRequestFilter.class.getName());
     private final AuthUserDetailsService authUserDetailsService;
     private final JWTUtils jwtUtils;
+
+    @Autowired
+    public JWTRequestFilter(AuthUserDetailsService authUserDetailsService, JWTUtils jwtUtils) {
+        this.authUserDetailsService = authUserDetailsService;
+        this.jwtUtils = jwtUtils;
+    }
 }
