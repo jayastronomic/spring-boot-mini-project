6a66f51eeb5701ae26b11ef7f4fdbe9e83dba68b
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 14:24:49 -0500

    Annotate Book model as Entity and a Table

--- src/main/java/com/example/library/models/Book.java
+++ src/main/java/com/example/library/models/Book.java
@@ -1,4 +1,12 @@
 package com.example.library.models;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "books")
 public class Book {
 }
