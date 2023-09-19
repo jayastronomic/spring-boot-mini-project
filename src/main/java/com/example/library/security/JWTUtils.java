a1616d5cc52ce078cbd3a3a78f450e46a8582885
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 22:59:09 -0500

    Create variables for JWTUtils

--- src/main/java/com/example/library/security/JWTUtils.java
+++ src/main/java/com/example/library/security/JWTUtils.java
@@ -1,9 +1,21 @@
 package com.example.library.security;
-import org.springframework.stereotype.Service;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import java.util.logging.Logger;
 
 
 @Service
 public class JWTUtils {
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpMS;
+
+
 
 }
