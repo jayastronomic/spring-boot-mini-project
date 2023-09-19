a915fdfbf6b0d52c5fc16f8f58d004ee28064142
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 17:41:13 -0500

    Create ExceptionHandler Class

--- src/main/java/com/example/library/controllers/BookController.java
+++ src/main/java/com/example/library/controllers/BookController.java
@@ -4,11 +4,16 @@ import com.example.library.models.Book;
 import com.example.library.services.BookService;
 import com.example.library.utils.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
