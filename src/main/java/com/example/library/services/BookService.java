69dddeae1b3903bf90f4356f5c252377bab006da
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 14:57:51 -0500

    Autowire dependicies to BookService

--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -1,8 +1,18 @@
 package com.example.library.services;
 
+import com.example.library.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
 
 @Service
 public class BookService {
+    Logger logger = Logger.getLogger(BookService.class.getName());
+    private final BookRepository bookRepository;
 
+    @Autowired
+    public BookService(BookRepository bookRepository){
+        this.bookRepository = bookRepository;
+    }
 }
