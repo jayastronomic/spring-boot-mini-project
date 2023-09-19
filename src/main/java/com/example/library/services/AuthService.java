621fa53ab37550579cc09e5fa7f25cc0c6394270
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 00:07:27 -0500

    Add fields to AuthService

--- src/main/java/com/example/library/services/AuthService.java
+++ src/main/java/com/example/library/services/AuthService.java
@@ -1,4 +1,14 @@
 package com.example.library.services;
 
+import com.example.library.models.User;
+import com.example.library.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
 public class AuthService {
+    Logger logger = Logger.getLogger(AuthService.class.getName());
+    private final UserRepository userRepository;
+
 }
