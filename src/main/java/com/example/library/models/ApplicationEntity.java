5f65e1411cae0ec1984a3f1f16b838ee1b1b867e
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:41:54 -0500

    Add createdAt field in ApplicationEntity class

--- src/main/java/com/example/library/models/ApplicationEntity.java
+++ src/main/java/com/example/library/models/ApplicationEntity.java
@@ -1,9 +1,12 @@
 package com.example.library.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -14,5 +17,9 @@ public class ApplicationEntity<T> {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public UUID id;
 
-
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
 }
