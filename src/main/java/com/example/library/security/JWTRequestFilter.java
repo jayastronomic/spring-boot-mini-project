7116d34a24eafd636328072853b8b682e9b19763
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:33:06 -0500

    Create JWTRequestFilter::doFilterInternal method

--- src/main/java/com/example/library/security/JWTRequestFilter.java
+++ src/main/java/com/example/library/security/JWTRequestFilter.java
@@ -1,14 +1,23 @@
 package com.example.library.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 @Component
-public class JWTRequestFilter {
+public class JWTRequestFilter extends OncePerRequestFilter {
     Logger logger = Logger.getLogger(JWTRequestFilter.class.getName());
     private final AuthUserDetailsService authUserDetailsService;
     private final JWTUtils jwtUtils;
@@ -28,4 +37,20 @@ public class JWTRequestFilter {
         logger.info("No header");
         return null;
     }
-}
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJWT(request);
+            if (jwt != null && jwtUtils.validateJWT(jwt)) {
+                String username = jwtUtils.getUserNameFromJWT(jwt);
+                UserDetails userDetails = this.authUserDetailsService.loadUserByUsername(username); // email address
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        } catch (Exception e) {
+            logger.info("cannot set user authentication token");
+        }
+        filterChain.doFilter(request, response);
+    }}
