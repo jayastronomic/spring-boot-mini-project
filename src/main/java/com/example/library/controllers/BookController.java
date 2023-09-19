278581771cc4ecfb9bd09957289a357d587bbf2c
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 15:17:41 -0500

    Create BookController::create method

--- src/main/java/com/example/library/controllers/BookController.java
+++ src/main/java/com/example/library/controllers/BookController.java
@@ -1,9 +1,16 @@
 package com.example.library.controllers;
 
+import com.example.library.models.Book;
 import com.example.library.services.BookService;
+import com.example.library.utils.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -13,5 +20,10 @@ public class BookController {
     @Autowired
     public BookController(BookService bookService){
         this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<APIResponse> create(@Valid @RequestBody Book book){
+        return bookService.createBook(book);
     }
 }
