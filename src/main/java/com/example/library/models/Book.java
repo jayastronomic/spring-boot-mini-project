99360a04137f2ac76d994ed93dca7fcda30b521e
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 14:30:18 -0500

    Add Columns for Book model

--- src/main/java/com/example/library/models/Book.java
+++ src/main/java/com/example/library/models/Book.java
@@ -2,6 +2,7 @@ package com.example.library.models;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -9,9 +10,18 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "books")
 public class Book extends ApplicationEntity<Book> {
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private Integer pages;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(nullable = false)
     private String summary;
+
+    @Column(nullable = false)
     private User user;
 }
