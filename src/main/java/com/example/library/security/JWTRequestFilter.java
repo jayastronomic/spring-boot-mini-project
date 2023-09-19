2a4e6fae511de5d9f15d29fe920b8a70d8b1a19d
jayastronomic <juliansmith94@hotmail.com>
2023-09-18 23:30:34 -0500

    Create JWTRequestFilter::parseJWT method

--- src/main/java/com/example/library/security/JWTRequestFilter.java
+++ src/main/java/com/example/library/security/JWTRequestFilter.java
@@ -2,7 +2,9 @@ package com.example.library.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.logging.Logger;
 
 @Component
@@ -15,5 +17,15 @@ public class JWTRequestFilter {
     public JWTRequestFilter(AuthUserDetailsService authUserDetailsService, JWTUtils jwtUtils) {
         this.authUserDetailsService = authUserDetailsService;
         this.jwtUtils = jwtUtils;
+    }
+
+    private String parseJWT(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if (StringUtils.hasLength(headerAuth) && headerAuth.startsWith("Bearer")) {
+            return headerAuth.substring(7);
+        }
+
+        logger.info("No header");
+        return null;
     }
 }
