99b17b6594d4c5768eaded1cdfaf880506b94007
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 16:15:59 -0500

    Create BookService:readBook method

--- src/main/java/com/example/library/services/BookService.java
+++ src/main/java/com/example/library/services/BookService.java
@@ -4,9 +4,12 @@ import com.example.library.models.Book;
 import com.example.library.repositories.BookRepository;
 import com.example.library.utils.APIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -25,4 +28,20 @@ public class BookService extends ApplicationService {
         return ResponseEntity
                 .ok(new APIResponse(bookRepository.save(book)));
     }
+
+    public ResponseEntity<APIResponse> readBook(UUID id){
+      Book book = currentUser().findBookById(id);
+      if(book != null) return ResponseEntity.ok(new APIResponse(book));
+      return bookNotFound(id);
+    }
+
+
+    private ResponseEntity<APIResponse> bookNotFound(UUID id){
+        Map<String,String> errors = new HashMap<>();
+        errors.put("message", "Book not found with ID = " +id);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new APIResponse(errors));
+    }
+
 }
