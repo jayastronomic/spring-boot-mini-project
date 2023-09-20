7e93943304a22961cbe8cfe1b24c219087e32f28
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 23:38:31 -0500

    Autowire GenreRepository to GenreService

---
+++ src/main/java/com/example/library/services/GenreService.java
@@ -0,0 +1,16 @@
+package com.example.library.services;
+
+import com.example.library.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GenreService {
+    public final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService( GenreRepository genreRepository){
+        this.genreRepository = genreRepository;
+    }
+
+}
