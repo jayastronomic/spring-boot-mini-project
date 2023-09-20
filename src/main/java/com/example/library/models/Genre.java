dd8338e05f80a961f24f9e1b1f29650fd53f8418
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 00:28:37 -0500

    Add Java Doc comments to Genre model

--- src/main/java/com/example/library/models/Genre.java
+++ src/main/java/com/example/library/models/Genre.java
@@ -7,23 +7,41 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.List;
 
+/**
+ * The Genre class represents a genre entity in the library application.
+ * It extends the ApplicationEntity class to inherit common fields and functionality.
+ */
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "genres")
 public class Genre extends ApplicationEntity<Genre> {
+
+    /**
+     * The name of the genre.
+     */
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Name can not be blank")
     private String name;
 
-
+    /**
+     * The user who manages the genre.
+     */
     @JoinColumn(name = "user_id", nullable = false)
     @ManyToOne
     private User user;
 
+    /**
+     * The list of books associated with this genre.
+     */
     @ManyToMany
     private List<Book> books;
 
+    /**
+     * Updates the genre entity with data from another genre payload.
+     *
+     * @param payload The Genre object (payload) containing updated data.
+     */
     @Override
     public void update(Genre payload) {
     }
