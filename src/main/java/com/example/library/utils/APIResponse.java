fca52957180e380dfeca9c80ced62c6435b0d4f3
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 07:45:43 -0500

    Add Java Doc comments to APIResponse

--- src/main/java/com/example/library/utils/APIResponse.java
+++ src/main/java/com/example/library/utils/APIResponse.java
@@ -1,3 +1,9 @@
 package com.example.library.utils;
 
+/**
+ * The APIResponse class represents the response body structure for API responses.
+ * It encapsulates the actual response data.
+ *
+ * @param responseBody The data or object to be included in the API response.
+ */
 public record APIResponse(Object responseBody) {}
