f084883b86b659bf7b91208c9afc7de44fd29a79
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 07:47:09 -0500

    Add Java Doc comments to Application

--- src/main/java/com/example/library/Application.java
+++ src/main/java/com/example/library/Application.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+/**
+ * The main application class that serves as the entry point for the library application.
+ */
 @SpringBootApplication
 @EnableJpaAuditing
 public class Application {
+
+
+	/**
+	 * The main method to start the library application.
+	 *
+	 * @param args The command-line arguments (if any).
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
