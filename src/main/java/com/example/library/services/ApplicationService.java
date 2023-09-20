2e77f69f6877019d9d8ae3148becb96683fda580
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 07:32:26 -0500

    Add Java Doc comments to ApplicationService

--- src/main/java/com/example/library/services/ApplicationService.java
+++ src/main/java/com/example/library/services/ApplicationService.java
@@ -5,8 +5,18 @@ import com.example.library.security.AuthUserDetails;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+/**
+ * The ApplicationService class provides common utility methods for accessing information about the current user
+ * within the library application.
+ */
 @Service
 public class ApplicationService {
+
+    /**
+     * Retrieves the currently authenticated user.
+     *
+     * @return The User object representing the currently authenticated user.
+     */
     public static User currentUser() {
         AuthUserDetails authUserDetails = (AuthUserDetails) SecurityContextHolder
                 .getContext()
