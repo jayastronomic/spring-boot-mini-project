0fe0030c5ae5fa8c0a853f4dab209f012d0a1a3d
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 00:31:24 -0500

    Add Java Doc comments to User model

--- src/main/java/com/example/library/models/User.java
+++ src/main/java/com/example/library/models/User.java
@@ -15,6 +15,10 @@ import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * The User class represents a user entity in the library application.
+ * It extends the ApplicationEntity class to inherit common fields and functionality.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -23,33 +27,56 @@ import java.util.UUID;
 @Entity
 @Table(name = "users")
 public final class User extends ApplicationEntity<User> {
+    /**
+     * The username of the user.
+     */
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Username can not be blank")
     @Size(min = 2, max = 300, message = "Username must be between 2 and 300 characters")
     private String username;
 
+    /**
+     * The email address of the user.
+     */
     @Column(nullable = false, unique = true)
     @NotBlank(message = "Email can not be blank")
     @Email(message = "Invalid email format")
     @Size(max = 300, message = "Email must be less than 300 characters")
     private String email;
 
+    /**
+     * The password of the user.
+     */
+
     @Column(nullable = false)
     @NotBlank(message = "Password can not be blank")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
+    /**
+     * The list of books associated with the user.
+     */
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "user", orphanRemoval = true)
     @LazyCollection(LazyCollectionOption.FALSE)
     private List<Book> books;
 
-
+    /**
+     * Updates the user entity with data from another user payload.
+     *
+     * @param payload The User object (payload) containing updated data.
+     */
     @Override
     public void update(User payload) {
         setUsername(payload.getUsername());
     }
 
+    /**
+     * Finds a book associated with the user by its unique identifier.
+     *
+     * @param id The unique identifier of the book to find.
+     * @return The Book object if found, or null if not found.
+     */
     public Book findBookById(UUID id){
         return books
                 .stream()
