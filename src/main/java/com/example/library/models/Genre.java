8459f09164061c1691397e9b4eb7bb9839d8a5c9
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 20:42:08 -0500

    Add column annotations to Genre

--- src/main/java/com/example/library/models/Genre.java
+++ src/main/java/com/example/library/models/Genre.java
@@ -1,17 +1,30 @@
 package com.example.library.models;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "genres")
 public class Genre extends ApplicationEntity<Genre> {
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Name can not be blank")
+    private String name;
+
+
+    @JoinColumn(name = "user_id", nullable = false)
+    @ManyToOne
+    private User user;
+
+    @ManyToMany
+    private List<Book> books;
 
     @Override
     public void update(Genre payload) {
-
     }
 }
