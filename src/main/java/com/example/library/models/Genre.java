84509d937de3ca25fa68a6904e5ab73368fee462
jayastronomic <juliansmith94@hotmail.com>
2023-09-19 20:27:12 -0500

    Extend Genre class with ApplicationEntity

--- src/main/java/com/example/library/models/Genre.java
+++ src/main/java/com/example/library/models/Genre.java
@@ -1,4 +1,17 @@
 package com.example.library.models;
 
-public class Genre {
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "genres")
+public class Genre extends ApplicationEntity<Genre> {
+
+    @Override
+    public void update(Genre payload) {
+
+    }
 }
