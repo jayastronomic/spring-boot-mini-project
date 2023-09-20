ddd5299bc4bdd41fd1cdf5c01d50360617303e79
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 07:41:12 -0500

    Add Java Doc comments to CorsPolicy

--- src/main/java/com/example/library/security/CorsPolicy.java
+++ src/main/java/com/example/library/security/CorsPolicy.java
@@ -5,9 +5,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * The CorsPolicy class configures Cross-Origin Resource Sharing (CORS) settings for the library application.
+ */
 @Configuration
 @EnableWebMvc
 public class CorsPolicy implements WebMvcConfigurer {
+
+    /**
+     * Configures CORS mappings to allow requests from a specified origin, with specified HTTP methods, headers,
+     * and credentials support.
+     *
+     * @param registry The CorsRegistry used to configure CORS settings.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
