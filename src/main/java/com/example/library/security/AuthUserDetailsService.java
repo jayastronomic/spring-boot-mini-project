f1ebc9b9e533ff461327706066b651c761a1692a
jayastronomic <juliansmith94@hotmail.com>
2023-09-20 07:39:34 -0500

    Add Java Doc comments to AuthUserDetailsService

--- src/main/java/com/example/library/security/AuthUserDetailsService.java
+++ src/main/java/com/example/library/security/AuthUserDetailsService.java
@@ -7,15 +7,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * The AuthUserDetailsService class is responsible for loading user details by their email during authentication.
+ */
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Autowired
-    public AuthUserDetailsService(UserRepository userRepository){
+    public AuthUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Loads user details by their email address.
+     *
+     * @param email The email address of the user to load.
+     * @return UserDetails containing user information.
+     * @throws UsernameNotFoundException If the user with the specified email is not found.
+     */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return new AuthUserDetails(userRepository.findByEmail(email));
